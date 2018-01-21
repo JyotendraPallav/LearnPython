a800db44a48d62b26a72ab552fe402e45fdd021e
JyotendraPallav <Jyotendra@gmail.com>
2018-01-21 23:26:46 +0530

    Completed finding Area

---
+++ Program4.py
@@ -0,0 +1,5 @@
+'''Program 4: Taking radius as an input and producing Area as output'''
+import math
+Radius=input('Input the radius of circle:' )
+print(type((math.pi*float(Radius)**2)))
+print("The Area of circle is:",round((math.pi*float(Radius)**2),2))
