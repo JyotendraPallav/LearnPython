7b2441735db57dafe097a86209445b5fb8c6b24c
JyotendraPallav <Jyotendra@gmail.com>
2018-01-21 13:09:03 +0530

    Completed Twinkle Twinkle

--- Program1.py
+++ Program1.py
@@ -1,1 +1,15 @@
 '''Writing my first program - Twinkle Twinkle Little Star'''
+'''\n\t\t\t\t above the world so high'''
+
+print('Twinkle, twinkle, little star',
+      '\n',
+      '\t\tHow I wonder what you are!'
+      '\n',
+      '\t\t\tUp above the world so high,',
+      '\n',
+      '\t\t\tLike a diamond in the sky.',
+      '\n',
+      'Twinkle, twinkle, little star',
+      '\n',
+      '\t\tHow I wonder what you are!')
+
