b7b59f08a0f245d466627ce0714363ee929ce398
JyotendraPallav <Jyotendra@gmail.com>
2018-01-24 23:21:53 +0530

    Added Program 5 for printing list and tuple

---
+++ Program5.py
@@ -0,0 +1,6 @@
+# Progam 5: Take input and create list, tuple
+
+List_of_Numbers = input("Please enter numbers: ")
+print(type(List_of_Numbers))
+print(List_of_Numbers)
+print(list(List_of_Numbers))
