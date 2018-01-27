481d674e4a4dd01acbffd4715dc7d7a87f08a0cc
JyotendraPallav <Jyotendra@gmail.com>
2018-01-27 12:31:52 +0530

    Added Program 13 for counting in a list (random number)

---
+++ Program13.py
@@ -0,0 +1,13 @@
+# Program 13 (22) - Count the number of 4 in a list
+# import random
+# In_List=random.sample(range(15),15)    Used for Random allocated number
+
+import numpy as np
+In_List = np.random.choice(10,10,replace=True)  # Used for Random numbers with repition
+
+counter=input("Please input the number for counting in random list: ")
+print("The number of occurence for %i in %r is: %i"%(counter,In_List,In_List.count(4)))
+
+
+# np is ndaraay - Will deal with it later but for now, count function counts occurence in 
+#a list (Gyaan)
