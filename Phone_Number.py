3e4dacbd25f5f64a826755b707be574c9640396c
JyotendraPallav <Jyotendra@gmail.com>
2018-03-11 22:38:48 +0530

    Regex learnt: Completed the Phone number case study

--- Phone_Number.py
+++ Phone_Number.py
@@ -1,7 +1,8 @@
 import re,string
 print("The idea of this code is to read all phone numbers and save them accordingly into number and extension")
-Phone_number =['800-555-1212','800 555 1212','800.555.1212','(800) 555-1212','1-800-555-1212','800-555-1212-1234','800-555-1212x1234','800-555-1212 ext. 1234','work 1-(800) 555.1212 #1234']
-print(type(Phone_number),str(Phone_number[1]))
-phonepattern=re.compile(r'(\d{3})-(\d{3})-(\d{4})$')
+Phone_number =['800-555-1212','800 555 1212','800.555.1212','(800) 555-1212','1-800-555-1212','800-555-1212-1234',
+'800-555-1212x1234','800-555-1212 ext. 1234','work 1-(800) 555.1212 #1234']
+#\D+(\d+)
+phonepattern=re.compile(r'(\d{3})\D*(\d{3})\D*(\d{4})\D*(\d*)$')
 for Pnumber in Phone_number:
     print(Pnumber,": ",phonepattern.search(Pnumber).groups())
